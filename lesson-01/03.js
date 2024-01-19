94a3897f77d639f3d33da2a4986b23a0d3fed02a
Nuraiym Nurbulatova <140681537+nooraiym@users.noreply.github.com>
2024-01-19 15:08:40 +0600

    fix week 1 - task 2

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,7 +1,7 @@
 // code 1,3,3,1
 let number = 1;
 
-console.log(number++); 
+console.log(number);
+console.log(number + ++number);
 console.log(++number); 
-console.log(number--); 
-console.log(--number); 
+console.log(number - number--); 
