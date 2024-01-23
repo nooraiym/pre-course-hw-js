0f2efa40f55eeb664b7dd7f1223994aa23411ec8
Nuraiym Nurbulatova <140681537+nooraiym@users.noreply.github.com>
2024-01-23 20:01:13 +0600

    add week 2 - 5 task

---
+++ lesson-02/05.js
@@ -0,0 +1,11 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let passport2 = {
+    name: "Ivan",
+    surname: "Petrov",
+};
+console.log(passport.name);
+console.log(passport2.name);
