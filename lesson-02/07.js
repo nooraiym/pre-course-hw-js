a54a4ef7d104c477ad3633f46a69a61bc089431d
Nuraiym Nurbulatova <140681537+nooraiym@users.noreply.github.com>
2024-01-24 11:39:27 +0600

    fix week 2 - 7 task

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -6,16 +6,9 @@ let passportMarried = {
         city: "Bobryisk"
     }
 };
-let passportMarried2 = {
-    name: "Petr",
-    surname: "Petrov",
-    married: true,
-    address: {
-        country: "USA",
-        city: "Bobryisk"
-    }
-};
 
+let passportMarried2 = passportMarried;
+passportMarried2.married =false;
 
 console.log(passportMarried); 
 console.log(passportMarried2);
