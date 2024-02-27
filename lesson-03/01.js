3243adc9135a674cc3bfce6e2ffb98aaea4f5b71
Nuraiym Nurbulatova <140681537+nooraiym@users.noreply.github.com>
2024-02-27 23:03:33 +0600

    change answer to lesson 3 - 1 task

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -2,7 +2,7 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-    return colors.filter(color => color == "черный" || color == "красный" || color == "желтый").join(', ');
+    return colors.filter(color => color == "черный" || color == "красный" || color == "желтый").join('-');
 }
 
 console.log(createColorString());
